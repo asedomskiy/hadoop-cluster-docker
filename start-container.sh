69ee47fd7b66df79f94dfa8728c3afbc5d17376b
asedomskiy <102352530+asedomskiy@users.noreply.github.com>
2022-03-27 19:54:35 +0300

    Update start-container.sh

--- start-container.sh
+++ start-container.sh
@@ -5,9 +5,9 @@ N=${1:-3}
 
 
 # start hadoop master container
-docker rm -f hadoop-master &> /dev/null
+sudo docker rm -f hadoop-master &> /dev/null
 echo "start hadoop-master container..."
-docker run -itd \
+sudo docker run -itd \
                 --net=hadoop \
                 -p 50070:50070 \
                 -p 8088:8088 \
@@ -20,12 +20,15 @@ docker run -itd \
 i=1
 while [ $i -lt $N ]
 do
-	docker rm -f hadoop-slave$i &> /dev/null
+	sudo docker rm -f hadoop-slave$i &> /dev/null
 	echo "start hadoop-slave$i container..."
-	docker run  \
+	sudo docker run -itd \
 	                --net=hadoop \
 	                --name hadoop-slave$i \
 	                --hostname hadoop-slave$i \
 	                kiwenlau/hadoop:1.0 &> /dev/null
 	i=$(( $i + 1 ))
 done 
+
+# get into hadoop master container
+sudo docker exec -it hadoop-master bash
